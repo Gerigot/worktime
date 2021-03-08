29d2ee6fae5cdff782edfb325779090da0225682
Gerigot <pat-1989@hotmail.com>
2021-03-08 09:22:13 +0100

    trying a new inserttime's label

--- src/TimeInsert/TimeInsert.js
+++ src/TimeInsert/TimeInsert.js
@@ -1,7 +1,27 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
+	timeContainer:{
+		position: 'relative',
+		'&:focus-within > $label':{
+			position: 'absolute',
+			top: '-0.75em',
+			left: '1em',
+			fontSize: '0.5rem',
+			zIndex: 2,
+			'&::before':{
+				content: '""',
+				position: 'absolute',
+				top: -10,
+				left: '-.5em',
+				right: '-.5em',
+				bottom: '0.25em',
+				background: 'var(--main-bg-color)',
+				zIndex: -1,
+			}
+		}
+	},
 	time: {
 		padding: 5,
 		fontSize: 'inherit',
@@ -11,19 +31,52 @@ const useStyles = createUseStyles({
 		backgroundColor: 'var(--input-bg-color)',
 		border: '2px solid var(--input-border-color)',
 		borderRadius: '5px',
-		marginBottom: '1em',
+		marginBottom: '1em', 
 		'&:focus': {
 			border: '2px solid var(--input-focus-border-color)'
 		}
-	}
+	},
+	label: (props)=>{
+		const common = {
+			transition: 'top 250ms ease, left 250ms ease, font-size 250ms ease'
+		};
+		if(props.value){
+			return {
+				...common,
+				position: 'absolute',
+				top: '-0.75em',
+				left: '1em',
+				zIndex: 2,
+				fontSize: '0.5rem',
+				'&::before':{
+					content: '""',
+					position: 'absolute',
+					top: -10,
+					left: '-.5em',
+					right: '-.5em',
+					bottom: '0.25em',
+					background: 'var(--main-bg-color)',
+					zIndex: -1,
+				}
+			};
+		}
+		return {
+			...common,
+			position: 'absolute',
+			fontSize: '0.8rem',
+			top: '0.5em',
+			left: '1em',
+			
+		}	
+	},
 });
 function TimeInsert({ value, onChange, label }) {
-	const classes = useStyles();
+	const classes = useStyles({value});
 	return (
-		<Fragment>
+		<div className={classes.timeContainer}>
 			<div className={classes.label}>{label}</div>
 			<input className={classes.time} value={value} onChange={verifyValue(onChange)} />
-		</Fragment>
+		</div>
 	);
 }
 
